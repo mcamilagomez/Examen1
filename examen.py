573eb45bed93bbf1c8eadb428f0212ca3df9ae50
mcamilagomez <98895057+mcamilagomez@users.noreply.github.com>
2022-04-06 10:41:04 -0500

    Add files via upload

---
+++ examen.py
@@ -0,0 +1,14 @@
+from typing_extensions import Self
+
+
+class calcularA (): 
+    pass
+class semiperimetro (nlados):
+    pass
+class apotema(nlados): 
+    pass 
+class nlados(Self): 
+    def __init__ (self, lados: int): 
+        self.lados = lados 
+    def tam(self, __init__, tamaño : int):
+        self.lados = tamaño
